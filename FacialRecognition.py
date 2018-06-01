08876f3d40ade503536a10e3e92f9892ee436a78
dhruv-dj <dhruv2703@gmail.com>
2018-06-01 13:33:39 +0530

    Face Tracking

---
+++ FacialRecognition.py
@@ -0,0 +1,30 @@
+import numpy as np
+import cv2
+
+face_cascade = cv2.CascadeClassifier('cascades/haarcascade_frontalface_alt2.xml')
+cap = cv2.VideoCapture(0)
+
+
+while True:
+    ret, frame = cap.read()
+    gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, scaleFactor = 1.5,minNeighbors = 5)
+
+    for (x,y,w,h) in faces:
+        print(x,y,w,h)
+        roi_gray=gray[y:y+h,:x+w]
+        img_item='my-image.png'
+        cv2.imwrite(img_item,roi_gray)
+        color = (0,0,255)
+        stroke = 2
+        end_x = x+w
+        end_y = y+h
+        cv2.rectangle(frame,(x,y),(end_x,end_y),color,stroke)
+    cv2.imshow('frame',frame)
+
+    if cv2.waitKey(20) & 0xFF == ord('q'):
+        break
+
+
+cap.release()
+cv2.destroyAllWindows()
